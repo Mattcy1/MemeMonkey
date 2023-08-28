51549b0c4d78633da8454ee4aca09fdbf53489a3
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-28 14:47:31 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,13 @@
+﻿namespace MemeTowerMod;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "37.3";
+    public const string Version = "1.0.0";
+    public const string Name = "Meme Tower";
+
+    public const string Description = "This a joke mod Credit to the creator of the meme";
+
+    public const string RepoOwner = "Mattcy1"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "Meme Tower"; // TODO add your repo name here, also in the download url in README.md
+}
